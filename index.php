80a680c3a601be86b56520dc89bb0680097546a1
Comandos <00comandos@gmail.com>
2014-11-08 18:45:30 -0430

    cambios1

--- index.php
+++ index.php
@@ -25,46 +25,11 @@
     
     
     
-    
-    
+  
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+
   </body>
-
-  <footer>
-      <div class="container">
-          <div class="row">
-            <div class="col-md-3">
-                <div class="logo-footer">
-                  <img src="img/logo-footer.png" alt="Ranyela Gómez - Accesorios">
-                </div>
-            </div>
-            <div class="col-md-3 col-md-offset-6">
-                <div class="social">
-                    <span id="titulo-sociales">¡Síguenos en las Redes Sociales</span>
-                    <div class="social-media">
-                        <a href="#"><img src="img/facebook-icon.png" alt=""></a>
-                        <a href="#"><img src="img/twitter-icon.png" alt=""></a>
-                        <a href="#"><img src="img/instagram-icon.png" alt=""></a>
-                    </div>
-                </div>
-                
-                
-            </div>
-          </div>
-
-          <!--
-              DERECHOS RESERVADOS
-          -->
-          <div class="row">
-              <div class="col-md-12">
-                <h5 id="derechos-reservados">Ranyela Gómez Accesorios - Copyright 2014 - Todos los Derechos Reservados - Hecho en Venezuela</h5>
-              </div>
-          </div>
-      </div>
-    
-  </footer>
+  <?php include 'inc/footer.php'; ?>
+  <?php include 'inc/footer_common.php'; ?>
+  
 </html>
